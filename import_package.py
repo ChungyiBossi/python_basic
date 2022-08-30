9c6db6b0c6f3100a681dfdc9b0eaefedb5e314e2
ChungyiBossi <chungyi.bossi@gmail.com>
2022-08-30 20:48:26 +0800

    [refactor] autopep8

--- import_package.py
+++ import_package.py
@@ -5,6 +5,6 @@
 # print_a()
 
 
-## 需要在 __init__.py 中先處理過後再使用
+# 需要在 __init__.py 中先處理過後再使用
 from python_lesson_package import print_a
 print_a()
--- tools.py
+++ tools.py
@@ -2,12 +2,15 @@ import math
 
 x = 9000
 y = 1500
-##　算第三邊
-def calculate_triangle_third_edge(edge_1, edge_2, is_third_edge_long_edge=True): 
+# 　算第三邊
+
+
+def calculate_triangle_third_edge(edge_1, edge_2, is_third_edge_long_edge=True):
     if is_third_edge_long_edge:
         return math.sqrt(edge_1*edge_1 + edge_2*edge_2)
     else:
         return math.sqrt(abs(edge_1*edge_1 - edge_2*edge_2))
+
 
 def use_args_kwargs(*args, **kwargs):
     for arg in args:
