27001782bd6ce9d6d192e7e715481dc3460a092b
ChungyiBossi <chungyi.bossi@gmail.com>
2022-09-06 21:29:33 +0800

    [feature] news web crawler

--- web_crawler.py
+++ web_crawler.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 
 html_doc = """\
-<html><head><title>The Dormouse's story</title></head>
+<html>
+<head><title>The Dormouse's story</title></head>
 <body>
 <p class="title"><b>The Dormouse's story</b></p>
 
